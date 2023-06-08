e278d748097fb7332d1f5d4906c55904023ce994
Larocca <Larocca@RIVPRO1.net>
2023-06-08 18:14:10 -0400

    Added Ahsoka info

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,22 @@ function App() {
       const timeDifference = endDate - now;
       const duration = intervalToDuration({ start: now, end: endDate });
 
-      const formattedTime = format(endDate, "MMMM do, yyyy h:mm a");
-      setCountdown(`${formatDuration(duration)} until ${formattedTime}`);
+      // const formattedTime = format(endDate, "MMMM do, yyyy h:mm a");
+      // setCountdown(`${formatDuration(duration)} until ${formattedTime}`);
+      setCountdown(`${formatDuration(duration)}`);
     }, 1000);
 
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div>
-      <h2>Countdown Timer</h2>
-      <p>{countdown}</p>
+    <div className='ctn-timer'>
+      <img src='https://m.media-amazon.com/images/M/MV5BYzA3ZjQ4YTItNjRhYS00YzRkLTg4NTEtMTYyOGZjNDFmYWM5XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg' width={`300px`}/> 
+      <div className='timer'>
+        <h2>Countdown Timer</h2>
+        <h3>{format(endDate, "MMMM do, yyyy")}</h3>
+        <h4>{countdown}</h4>
+      </div>
     </div>
   );    
 }
