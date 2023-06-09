710275e4e25c95b4befc181c560688e43babc265
Larocca <Larocca@RIVPRO1.net>
2023-06-09 16:23:58 -0400

    Added 1 day to rendered days element

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { format, formatDuration, intervalToDuration, isBefore } from 'date-fns';
+import { format, formatDuration, intervalToDuration, isBefore, addDays } from 'date-fns';
 import Confetti from 'react-confetti';
 
 import './App.css'
@@ -27,9 +27,10 @@ function App() {
   }, [endDate]);
 
   const handleDateChange = (event) => {
-    const chosenDate = new Date(event.target.value);
+    // const chosenDate = new Date(event.target.value);
+    const chosenDate = new Date(event.target.value + 'T00:00:00');
     const date = new Date(event.target.value);
-    date.setDate(date.getDate() + 1);
+    // date.setDate(date.getDate() + 1);
     setEndDate(date);
     setCountdownEnded(false);
   };
@@ -41,7 +42,8 @@ function App() {
       <div className='timer'>
         <h2>Countdown Clock</h2>
         <input type="date" onChange={handleDateChange} />
-        <h3>{format(endDate, "MMMM do, yyyy")}</h3>
+        {/* <h3>{format(endDate, "MMMM do, yyyy")}</h3> */}
+        <h3>{format(addDays(endDate, 1), "MMMM do, yyyy")}</h3>
         {!countdownEnded && <h4>{countdown}</h4>}
         {countdownEnded && <h4>Countdown Ended!</h4>}
       </div>
