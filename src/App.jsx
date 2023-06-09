3607a0dd6f2b696cc880dc73dfe4e6677492bbf7
Larocca <Larocca@RIVPRO1.net>
2023-06-09 15:25:37 -0400

    Added date picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,49 @@
 import { useState, useEffect } from 'react';
-import { format, formatDuration, intervalToDuration } from 'date-fns';
+import { format, formatDuration, intervalToDuration, isBefore } from 'date-fns';
+import Confetti from 'react-confetti';
 
 import './App.css'
 
 function App() {
-  const endDate = new Date("August 23, 2023");
+  const [endDate, setEndDate] = useState(new Date("August 23, 2023"));
   const [countdown, setCountdown] = useState('');
+  const [countdownEnded, setCountdownEnded] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
       const now = new Date();
-      const timeDifference = endDate - now;
       const duration = intervalToDuration({ start: now, end: endDate });
 
-      // const formattedTime = format(endDate, "MMMM do, yyyy h:mm a");
-      // setCountdown(`${formatDuration(duration)} until ${formattedTime}`);
-      setCountdown(`${formatDuration(duration)}`);
+      if (isBefore(endDate, now)) { 
+        setCountdownEnded(true);
+        clearInterval(interval);
+      } else { 
+        setCountdown(`${formatDuration(duration)}`);
+      }
+
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [endDate]);
+
+  const handleDateChange = (event) => {
+    const chosenDate = new Date(event.target.value);
+    const date = new Date(event.target.value);
+    date.setDate(date.getDate() + 1);
+    setEndDate(date);
+    setCountdownEnded(false);
+  };
 
   return (
     <div className='ctn-timer'>
+      {countdownEnded && <Confetti />}
       <img src='https://m.media-amazon.com/images/M/MV5BYzA3ZjQ4YTItNjRhYS00YzRkLTg4NTEtMTYyOGZjNDFmYWM5XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg' width={`300px`}/> 
       <div className='timer'>
-        <h2>Countdown Timer</h2>
+        <h2>Countdown Clock</h2>
+        <input type="date" onChange={handleDateChange} />
         <h3>{format(endDate, "MMMM do, yyyy")}</h3>
-        <h4>{countdown}</h4>
+        {!countdownEnded && <h4>{countdown}</h4>}
+        {countdownEnded && <h4>Countdown Ended!</h4>}
       </div>
     </div>
   );    
