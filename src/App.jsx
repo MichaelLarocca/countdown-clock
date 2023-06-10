55e9f299b92ef21c340cc0bd333676c7a3402664
Larocca <Larocca@RIVPRO1.net>
2023-06-10 10:09:46 -0400

    Added local storage feature, and updated date picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useState, useEffect } from 'react';
 import { format, formatDuration, intervalToDuration, isBefore, add } from 'date-fns';
 import Confetti from 'react-confetti';
-
 import './App.css'
 
 function App() {
-  const [endDate, setEndDate] = useState(new Date("August 23, 2023"));
   const [countdown, setCountdown] = useState('');
   const [countdownEnded, setCountdownEnded] = useState(false);
+  const [endDate, setEndDate] = useState(() => {
+    const initialEndDate = localStorage.getItem("endDate");
+    return initialEndDate ? new Date(Date.parse(initialEndDate)) : new Date("August 23, 2023");
+  });
+
+  const handleDateChange = (event) => {
+    const chosenDate = new Date(event.target.value);
+    const dateEST = add(chosenDate, { hours: 4 });
+    localStorage.setItem("endDate", dateEST);
+    setEndDate(dateEST);
+    setCountdownEnded(false);
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -26,21 +36,13 @@ function App() {
     return () => clearInterval(interval);
   }, [endDate]);
 
-  const handleDateChange = (event) => {
-    const chosenDate = new Date(event.target.value);
-    const date = new Date(event.target.value);
-    const dateEST = add(date, { hours: 4 })
-    setEndDate(dateEST);
-    setCountdownEnded(false);
-  };
-
   return (
     <div className='ctn-timer'>
       {countdownEnded && <Confetti />}
       <img src='https://m.media-amazon.com/images/M/MV5BYzA3ZjQ4YTItNjRhYS00YzRkLTg4NTEtMTYyOGZjNDFmYWM5XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg' width={`300px`}/> 
       <div className='timer'>
         <h2>Countdown Clock</h2>
-        <input type="date" onChange={handleDateChange} />
+        <input type="date" min={format(new Date(), "yyyy-MM-dd")} onChange={handleDateChange} />
         <h3>{format(endDate, "MMMM do, yyyy")}</h3>
         {!countdownEnded && <h4>{countdown}</h4>}
         {countdownEnded && <h4>Countdown Ended!</h4>}
